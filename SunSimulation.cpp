ae6953d8caa40eba564bce1ce50488e39acadebf
artem <arbotnev@yandex.ru>
2018-10-17 22:24:57 +0300

    added init data check

--- SunSimulation.cpp
+++ SunSimulation.cpp
@@ -4,21 +4,78 @@
 
 #include "SunSimulation.h"
 
-TimeSet::TimeSet(short hour, short minute, short duration) {
+TimeSet::TimeSet(uint8_t hour, uint8_t minute, uint16_t duration) {
     _hour = hour;
     _minute = minute;
     _duration = duration;
 }
 
-short TimeSet::getHour() {
+uint8_t TimeSet::getHour() {
     return _hour;
 }
 
-short TimeSet::getMinute() {
+uint8_t TimeSet::getMinute() {
     return _minute;
 }
 
-short TimeSet::getDuration() {
+uint16_t TimeSet::getDuration() {
     return _duration;
+}
+
+SunSimulation::SunSimulation(TimeSet *sunRise, TimeSet *sunSet) {
+    _sunRise = sunRise;
+    _sunSet = sunSet;
+}
+
+char *SunSimulation::regimenInit() {
+    if (haveTimeError(_sunRise) || haveTimeError(_sunSet)) {
+        return "time input error\0";
+    }
+
+    // get time (seconds) since sunrise start
+    _endRise = _sunRise->getDuration() * 60;
+    _startSet = getSecondsFromStart(_sunRise, _sunSet->getHour(), _sunSet->getMinute(), 0);
+
+    if (_startSet < 60) {
+        return "time input error, too small a gap between sunrise and sunset\0";
+    }
+
+    _endSet = _startSet + _sunSet->getDuration() * 60;
+
+    if (_endSet > DAY_SECONDS || _endRise > _startSet) {
+        return "time input error, intersection of sunrise and sunset regiments\0";
+    }
+
+    _optionsOk = true;
+    _startPoint = _sunRise;
+
+    return "successful initialization\0";
+}
+
+bool SunSimulation::haveTimeError(TimeSet *set) {
+    return set->getHour() > 23 || set->getHour() < 0 || set->getMinute() > 59 ||
+           set->getMinute() < 0 || set->getDuration() > 120 || set->getDuration() < 5;
+}
+
+uint32_t SunSimulation::
+getSecondsFromStart(TimeSet *sunRise, uint32_t curHour, uint32_t curMinute, uint32_t curSecond) {
+    // seconds
+    uint32_t gap;
+
+    if (sunRise->getHour() < curHour) {
+        gap = ((curHour - sunRise->getHour()) * 60 + curMinute - sunRise->getMinute()) * 60
+              + curSecond;
+    } else if (sunRise->getHour() > curHour) {
+        gap = ((24 - sunRise->getHour() + curHour) * 60 + curMinute - sunRise->getMinute())
+              * 60 + curSecond;
+    } else {
+        if (sunRise->getMinute() <= curMinute) {
+            gap = curMinute * 60 + curSecond - sunRise->getMinute() * 60;
+        } else {
+            gap = (24 * 60 - ((sunRise->getMinute() - curMinute))) * 60 - curSecond;
+        }
+    }
+
+    return gap;
 }
 
--- SunSimulation.h
+++ SunSimulation.h
@@ -5,22 +5,46 @@
 #ifndef SUNSIMULATION_H
 #define SUNSIMULATION_H
 
+#include "inttypes.h"
+
+#define START_RISE 0
 #define PWM_MAX 255
+#define DAY_SECONDS 86400
 
 class TimeSet {
 public:
-    TimeSet(short hour, short minute, short duration);
+    TimeSet(uint8_t hour, uint8_t minute, uint16_t duration);
 
-    short getHour();
+    uint8_t getHour();
 
-    short getMinute();
+    uint8_t getMinute();
 
-    short getDuration();
+    uint8_t getDuration();
 
 private:
-    short _hour;
-    short _minute;
-    short _duration;
+    uint8_t _hour;
+    uint8_t _minute;
+    uint16_t _duration;
+};
+
+class SunSimulation {
+public:
+    SunSimulation(TimeSet *sunRise, TimeSet *sunSet);
+    char *regimenInit();
+
+private:
+    TimeSet *_sunRise;
+    TimeSet *_sunSet;
+    TimeSet *_startPoint;
+    // seconds
+    uint32_t _endRise;
+    uint32_t _startSet;
+    uint32_t _endSet;
+    // true if there is not initialization errors
+    bool _optionsOk;
+
+    bool haveTimeError(TimeSet *set);
+    uint32_t getSecondsFromStart(TimeSet *sunRise, uint32_t curHour, uint32_t curMinute, uint32_t curSecond);
 };
 
 #endif
